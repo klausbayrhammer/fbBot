67df0adb16304f78caffdcb70b64a3b5e8ab19ba
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2016-05-28 15:49:50 +0200

    respond to message

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
+const request = require('request');
 
 var bodyParser = require('body-parser');
+const APP_TOKEN = 'thisIsTheHotShit';
 
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
@@ -12,10 +14,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'thisIsTheHotShit') {
+    if (req.query['hub.verify_token'] === APP_TOKEN) {
         res.send(req.query['hub.challenge']);
     }
- console.log(req);
     res.send('Error, wrong validation token');
 });
 
@@ -26,11 +27,33 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id;
         if (event.message && event.message.text) {
             text = event.message.text;
+            sendTextMessage(sender, text)
             console.log(event);
         }
     }
     res.sendStatus(200);
 });
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:`callback ${text}`
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:APP_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
 
 var server = app.listen(process.env.PORT || 3000,() => {
     var host = server.address().address;
