b111dc6334bc121c697f1b5004a358f2e91e6e16
Klaus Bayrhammer <klaus.bayrhammer@at.redbullmediahouse.com>
2016-05-28 11:32:12 +0200

    listen for messages

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get('/webhook/', function (req, res) {
  console.log(req);
     res.send('Error, wrong validation token');
 });
+
+app.post('/webhook/', function (req, res) {
+    console.log('POST')
+    console.log(req)
+    res.sendStatus(200);
+});
+
 var server = app.listen(process.env.PORT || 3000,() => {
     var host = server.address().address;
     var port = server.address().port;
