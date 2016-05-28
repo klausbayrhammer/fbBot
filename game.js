131118c05b02f0f8941760485f988ae4a2558cde
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2016-05-28 19:57:00 +0200

    logs sendermap

--- game.js
+++ game.js
@@ -10,10 +10,10 @@ module.exports = (sender, event) => {
     } else if (senderMap[sender].level === levels.length-1) {
         sendTextMessage(sender, 'Great you did it');
     } else {
-        var currentLevel = senderMap[sender].level;
-        levels[currentLevel].answer(sender, event).then(successfullyDone => {
+        console.log(senderMap);
+        levels[senderMap[sender].level].answer(sender, event).then(successfullyDone => {
             if (successfullyDone) {
-                senderMap[sender].level = currentLevel+1;
+                senderMap[sender].level = senderMap[sender].level+1;
                 levels[senderMap[sender].level].question(sender, event);
             } else {
                 sendTextMessage(sender, 'Sorry wrong answer');
