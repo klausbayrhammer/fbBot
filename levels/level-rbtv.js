4209bab58e93ffa8b41056a7ff471933cb829633
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2016-05-29 09:14:31 +0200

    adds levenshtein string comparison

--- levels/level-rbtv.js
+++ levels/level-rbtv.js
@@ -1,11 +1,14 @@
 const getNextRBTvShow = require('../utils').getNextRBTvShow;
 const sendTextMessage = require('../utils').sendTextMessage;
+const levenshtein = require('fast-levenshtein');
 
 module.exports = {
     question: (sender, event) => {
         sendTextMessage(sender, 'What is the next upcoming Show on redbull.tv');
     },
     answer: (sender, event) => {
-        return getNextRBTvShow().then(showTitle => showTitle === event.message.text);
+        return getNextRBTvShow().then(showTitle => ({
+            success: levenshtein.get(showTitle.toUpperCase(), event.message.text.toUpperCase()) < 4
+        }));
     }
 };
